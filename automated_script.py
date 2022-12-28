6fb8d75a3d217f444e16f76069980530a61cca82
Hussein Ahmed Shahab <hussein.a.shahab@gmail.com>
2022-12-28 13:22:17 +0300

    Update automated_script.py

--- automated_script.py
+++ automated_script.py
@@ -4,9 +4,9 @@ import subprocess
 subprocess.run(["pip", "install", "ansible"])
 
 # Download the playbook from the URL
-subprocess.run(["curl", "-O", "https://raw.githubusercontent.com/husseinahmed-dev/Research-Project-1---Kubernetes-Hardening/main/01-deploy-loadbalancer.yml"])
-subprocess.run(["curl", "-O", "https://raw.githubusercontent.com/husseinahmed-dev/Research-Project-1---Kubernetes-Hardening/main/02-prepare-master-worker.yml"])
-subprocess.run(["curl", "-O", "https://raw.githubusercontent.com/husseinahmed-dev/Research-Project-1---Kubernetes-Hardening/main/03-install-kubernetes-master-worker.yml"])
+subprocess.run(["curl", "-O", "https://raw.githubusercontent.com/husseinahmed-dev/Research-Project-1-Kubernetes-Hardening/main/01-deploy-loadbalancer.yml"])
+subprocess.run(["curl", "-O", "https://raw.githubusercontent.com/husseinahmed-dev/Research-Project-1-Kubernetes-Hardening/main/02-prepare-master-worker.yml"])
+subprocess.run(["curl", "-O", "https://raw.githubusercontent.com/husseinahmed-dev/Research-Project-1-Kubernetes-Hardening/main/03-install-kubernetes-master-worker.yml"])
 
 # Apply the playbook using ansible-playbook
 subprocess.run(["ansible-playbook", "01-deploy-loadbalancer.yml"])
