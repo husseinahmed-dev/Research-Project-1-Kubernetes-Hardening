3252314fb8338f8cc1af336f3d21336f59d09446
Hussein Ahmed Shahab <hussein.a.shahab@gmail.com>
2022-12-28 13:08:32 +0300

    Automated Deployment with Python
    Automated Ansible Installation & Playbook configuration using python script for bootstrapping the servers.

---
+++ automated_script.py
@@ -0,0 +1,14 @@
+import subprocess
+
+# Install Ansible using pip
+subprocess.run(["pip", "install", "ansible"])
+
+# Download the playbook from the URL
+subprocess.run(["curl", "-O", "https://raw.githubusercontent.com/husseinahmed-dev/Research-Project-1---Kubernetes-Hardening/main/01-deploy-loadbalancer.yml"])
+subprocess.run(["curl", "-O", "https://raw.githubusercontent.com/husseinahmed-dev/Research-Project-1---Kubernetes-Hardening/main/02-prepare-master-worker.yml"])
+subprocess.run(["curl", "-O", "https://raw.githubusercontent.com/husseinahmed-dev/Research-Project-1---Kubernetes-Hardening/main/03-install-kubernetes-master-worker.yml"])
+
+# Apply the playbook using ansible-playbook
+subprocess.run(["ansible-playbook", "01-deploy-loadbalancer.yml"])
+subprocess.run(["ansible-playbook", "02-prepare-master-worker.yml"])
+subprocess.run(["ansible-playbook", "03-install-kubernetes-master-worker.yml"])
